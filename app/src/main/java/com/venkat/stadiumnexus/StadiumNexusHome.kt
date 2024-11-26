3a68158916f19319813ac08835e2e15395356166
Venkat1155 <nerusuv3@gmail.com>
2024-11-26 12:05:55 +0000

    sprint3 code

--- app/src/main/java/com/venkat/stadiumnexus/StadiumNexusHome.kt
+++ app/src/main/java/com/venkat/stadiumnexus/StadiumNexusHome.kt
@@ -1,8 +1,231 @@
 package com.venkat.stadiumnexus
+import android.content.Intent
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.venkat.stadiumnexus.ui.theme.StadiumNexusTheme
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 
 class AddtoCartActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StadiumNexusTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .requiredWidth(width = 360.dp)
+            .requiredHeight(height = 640.dp)
+            .background(color = Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(x = (-7).dp,
+                    y = (-86).dp)
+                .requiredWidth(width = 375.dp)
+                .requiredHeight(height = 812.dp)
+                .background(color = Color.White)
+        ) {
+
+            Text(
+                text = "£ 310.00",
+                color = Color(0xFF9458D1),
+                textAlign = TextAlign.End,
+                style = TextStyle(
+                    fontSize = 30.sp),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 241.dp,
+                        y = 691.dp))
+            Text(
+                text = "£ 10.00",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 20.sp),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 295.dp,
+                        y = 641.dp))
+            Text(
+                text = "£ 300.00",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 20.sp),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 280.dp,
+                        y = 601.dp))
+
+            Text(
+                text = "Total Amount",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 14.sp),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 701.dp))
+            Text(
+                text = "tax",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 14.sp),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 645.dp))
+            Text(
+                text = "Ticket Price",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 14.sp),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 605.dp))
+
+            val context = LocalContext.current
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 758.dp)
+                    .requiredWidth(width = 321.dp)
+                    .requiredHeight(height = 50.dp)
+                    .clickable {
+                        context.startActivity(
+                            Intent(context, PaymentActivity::class.java))
+                    }
+
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(color = Color(0xFF00B407)))
+            Text(
+                text = "Proceed to Checkout",
+                color = Color.White,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 108.dp,
+                        y = 773.dp))
+            Image(
+                painter = painterResource(id = R.drawable.cs),
+                contentDescription = "",
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 0.dp,
+                        y = 88.dp)
+                    .requiredWidth(width = 375.dp)
+                    .requiredHeight(height = 463.dp))
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 369.dp)
+                    .requiredWidth(width = 321.dp)
+                    .requiredHeight(height = 50.dp)
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(color = Color(0xffe8e8e8)))
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 258.dp,
+                        y = 374.dp)
+                    .requiredWidth(width = 85.dp)
+                    .requiredHeight(height = 40.dp)
+                    .clip(shape = RoundedCornerShape(11.dp))
+                    .background(color = Color(0xFF00B407)))
+            Text(
+                text = "Apply",
+                color = Color.White,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 279.dp,
+                        y = 384.dp))
+            Text(
+                text = "Promo Code",
+                color = Color.Black.copy(alpha = 0.2f),
+                style = TextStyle(
+                    fontSize = 14.sp),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 46.dp,
+                        y = 385.dp)
+                    .requiredWidth(width = 86.dp))
+            Text(
+
+                text = "This is a Cricket stadium which is - \n" +
+
+                        "established around 1950. This ground is  \n" +
+
+                        "known as best in the world and largest ground around the world. \n"
+                ,
+
+                color = Color.Black,
+                textDecoration = TextDecoration.Underline,
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium
+                ),
+                modifier = Modifier.padding(top = 16.dp)
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 5.dp,
+                        y = 451.dp))
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    StadiumNexusTheme {
+        Greeting2("Android")
+    }
 }
 
